7fb937b5f1a3bb9b26d169ed573bbdf5be7a2d35
Enrique Budiono <enriquebudiono@gmail.com>
2021-02-04 22:59:43 -0600

    part 2 code

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Enrique A. Budiono on 2/4/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
